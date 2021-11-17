a06ba610185212d33f3e9978dfe8b0910c2a14cf
mnecas <necas.marty@gmail.com>
2021-11-17 15:21:39 +0100

    Add Person model

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # bpc_bds_web
+
+`python3 manage.py migrate`
+
+`python3 manage.py createsuperuser`
+
+`python3 manage.py runserver`
--- main/models.py
+++ main/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Person(models.Model):
+    first_name = models.CharField(max_length=45)
+    last_name = models.CharField(max_length=45)
+    passowrd = models.CharField(max_length=45)
+    username = models.CharField(max_length=45)
+    date_of_birth = models.DateField()
+
