f792be60d266de4f9d35bf9b55b5c84b3673e662
mnecas <necas.marty@gmail.com>
2021-11-28 23:58:05 +0100

    Add user tests

--- main/tests.py
+++ main/tests.py
@@ -1,3 +1,23 @@
 from django.test import TestCase
+from main.models import Person
+from django.contrib.auth.models import User
 
-# Create your tests here.
+
+class UserTestCase(TestCase):
+    def setUp(self):
+        u = User.objects.create(
+            username='username',
+            first_name='first_name',
+            last_name='last_name',
+        )
+        u.set_password('password')
+        u.save()
+        Person.objects.create(
+            user=u,
+            date_of_birth='1999-10-07',
+            type='user'
+        )
+
+    def test_user_creation(self):
+        person = Person.objects.get(id=1)
+        self.assertEqual(person.user.username, 'username')
